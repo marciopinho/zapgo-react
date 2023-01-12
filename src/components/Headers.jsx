bc0ad6de1cdca57d829ec08d8a1940a8adbb597d
Mars Pinho <marciopinho@gmail.com>
2023-01-12 16:52:51 -0300

    headers now render in portuguese if country is BR, english else

--- src/components/Headers.jsx
+++ src/components/Headers.jsx
@@ -1,9 +1,35 @@
+import { useEffect, useState } from 'react'
+
 export default function Headers() {
+  const [userCountry, setUserCountry] = useState('')
+  useEffect(() => {
+    getCountryIsoCode('https://ipapi.co/json/?fields=61439')
+  }, [])
+
+  const getCountryIsoCode = async (url) => {
+    let data;
+    const res = await fetch(url)
+    data = await res.json();    
+    setUserCountry(data.country)
+  }
+
+  let headerTwo = "Send WhatsApp messages to unsaved contacts.";
+  let headerThree = "Enter a phone number and hit send to redirect to the app or WhatsApp web.";
+
+  if(userCountry === 'BR') {
+    headerTwo = 'Envie uma mensagem de WhatsApp sem salvar o contato.'
+    headerThree = 'Cole o número e aperte o botão para ser redirecionado para o app ou WhatsApp web.'
+  }
+  if(userCountry === 'IS') {
+    headerTwo = "Send WhatsApp messages to unsaved contacts.";
+    headerThree = "Enter a phone number and hit send to redirect to the app or WhatsApp web.";
+  }
+
   return (
     <>
-      <h2>Envie uma mensagem de WhatsApp sem salvar o contato.</h2>
+      <h2>{headerTwo}</h2>
       <div className="instructions">
-        <h3>Cole o número e aperte o botão para ser redirecionado para o app ou WhatsApp web.</h3>
+        <h3>{headerThree}</h3>
       </div>
     </>
   )
